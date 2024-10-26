0d894e9ec2bdc8879ddbb3e0340151454d3849ee
Jakub Niewelt <jakubniewelt@icloud.com>
2024-10-26 22:50:30 +0200

    fix: rename JwtSettings.cs prop

--- eUNI-API/Configuration/JwtSettings.cs
+++ eUNI-API/Configuration/JwtSettings.cs
@@ -5,5 +5,5 @@ public class JwtSettings
    public string Issuer { get; set; }
    public string Audience { get; set; }
    public int AccessTokenExpirationMinutes { get; set; }
-   public int RefreshTokenExpirationMinutes { get; set; }
+   public int RefreshTokenExpirationDays { get; set; }
 }
