68c2fc7350707f7f4e5f6d97a7d7460a72f95453
Jakub Niewelt <jakubniewelt@icloud.com>
2024-11-10 19:50:58 +0100

    feat: seed Db

--- eUNI-API/Data/AppDbContext.cs
+++ eUNI-API/Data/AppDbContext.cs
@@ -1,5 +1,7 @@
 using eUNI_API.Enums;
 using eUNI_API.Models.Entities.Auth;
+using eUNI_API.Models.Entities.FieldOfStudy;
+using eUNI_API.Models.Entities.OrganizationInfo;
 using eUNI_API.Models.Entities.Student;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,11 +10,29 @@ namespace eUNI_API.Data;
 public class AppDbContext: DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { } 
+    
+    // Auth
     public DbSet<User> Users { get; set; }
     public DbSet<Role> Roles { get; set; }
     public DbSet<PasswordResetLog> PasswordResetLogs { get; set; }
     public DbSet<RefreshToken> RefreshTokens { get; set; }
+    
+    //Student
+    public DbSet<Group> Groups { get; set; }
     public DbSet<Student> Students { get; set; }
+    public DbSet<StudentFieldsOfStudyLog> StudentFieldsOfStudyLogs { get; set; }
+    public DbSet<StudentGroup> StudentGroups { get; set; }
+    
+    //OrganizationInfo
+    public DbSet<DayOff> DaysOff { get; set; }
+    public DbSet<Hour> Hours { get; set; }
+    public DbSet<OrganizationOfTheYear> OrganizationsOfTheYear { get; set; }
+    public DbSet<Year> Years { get; set; }
+    
+    //FieldOfStudy
+    public DbSet<Class> Classes { get; set; }
+    public DbSet<FieldOfStudy> FieldOfStudies { get; set; }
+    public DbSet<FieldOfStudyLog> FieldOfStudyLogs { get; set; }
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -43,7 +63,131 @@ public class AppDbContext: DbContext
                 Salt = "f/5Yd/pRp65N6nbMHH8wlg=="
             }
         );
+
+        modelBuilder.Entity<Group>().HasData(
+            new Group
+            {
+                Id = 1,
+                Abbr = "P01"
+            },
+            new Group
+            {
+                Id = 2,
+                Abbr = "W"
+            },
+            new Group
+            {
+                Id = 3,
+                Abbr = "K2"
+            },
+            new Group
+            {
+                Id = 4,
+                Abbr = "K1"
+            }
+        );
+
+        modelBuilder.Entity<Hour>().HasData(
+            new Hour
+            {
+                Id = 1,
+                HourInterval="7:30 - 8:15"
+            },
+            new Hour
+            {
+                Id = 2,
+                HourInterval="8:15 - 9:00"
+            },
+            new Hour
+            {
+                Id = 3,
+                HourInterval="9:15 - 10:00"
+            },
+            new Hour
+            {
+                Id = 4,
+                HourInterval="10:00 - 10:45"
+            }
+        );
         
-         
+        modelBuilder.Entity<Year>().HasData(
+            new Year
+            {
+                Id = 1,
+                Name="2024/2025"
+            }
+        );
+
+        modelBuilder.Entity<OrganizationOfTheYear>().HasData(
+            new OrganizationOfTheYear
+            {
+                Id = 1,
+                YearId = 1,
+                FirstHalfOfYear = true,
+                StartDay = new DateOnly(2024, 10, 1),
+                EndDay = new DateOnly(2025, 1, 26),
+            }    
+        );
+
+        modelBuilder.Entity<FieldOfStudy>().HasData(
+            new FieldOfStudy
+            {
+                Id = 1,
+                Abbr="K",
+                Name="Informatyka Stosowana",
+                StudiesCycle = 1,
+            }
+        );
+
+        modelBuilder.Entity<FieldOfStudyLog>().HasData(
+            new FieldOfStudyLog
+            {
+                Id = 1,
+                FieldOfStudyId = 1,
+                OrganizationsOfTheYearId = 1,
+                Semester = 7
+            }    
+        );
+
+        modelBuilder.Entity<Class>().HasData(
+            new Class
+            {
+                Id = 1,
+                FieldOfStudyLogId = 1,
+                Name = "Wytrzymałość materiałów",
+                Room="A103",
+                IsOddWeek = true,
+                WeekDay = 0,
+                StartHourId = 1,
+                EndHourId = 3,
+            }    
+        );
+
+        modelBuilder.Entity<Student>().HasData(
+            new Student
+            {
+                Id = 1,
+                UserId = new Guid("dd205297-4d6d-4ef3-a139-350c55518085"),
+                AlbumNumber = "144863"
+            }
+        );
+        
+        modelBuilder.Entity<StudentFieldsOfStudyLog>().HasData(
+            new StudentFieldsOfStudyLog
+            {
+                Id = 1,
+                FieldsOfStudyLogId = 1,
+                StudentId = 1
+            }    
+        );
+        
+        modelBuilder.Entity<StudentGroup>().HasData(
+            new StudentGroup{
+                Id = 1,
+                GroupId = 1,
+                IsRepresentative = false,
+                StudentsFieldsOfStudyLogId = 1,
+            }
+        );
     }
 }
