45d7899e16315a052857aca4f62762b0155d6fa9
Jakub Niewelt <jakubniewelt@icloud.com>
2024-11-29 15:59:14 +0100

    fix: remove IsOddWeek and WeekDay from Class entity

--- eUNI-API/Models/Entities/FieldOfStudy/Class.cs
+++ eUNI-API/Models/Entities/FieldOfStudy/Class.cs
@@ -19,10 +19,6 @@ public class Class
     
     [MaxLength(15)]
     public string Room { get; set; }
-    
-    public bool? IsOddWeek { get; set; }
-    
-    public WeekDay? WeekDay { get; set; }
 
     public int GroupId { get; set; }
     
